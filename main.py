53761273129bca6f85f6410304cdbad99dc767e2
Michael Drummond <michael.drummond328@gmail.com>
2022-11-21 22:21:10 -0700

    added pages for classes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import time
 from turtle import Turtle, Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
 screen = Screen()
 
@@ -8,7 +11,9 @@ screen.bgcolor("black")
 screen.tracer(0)
 
 game_is_running = True
-
 while game_is_running:
     time.sleep(0.1)
     screen.update()
+
+
+screen.exitonclick()
